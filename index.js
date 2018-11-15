4f0cf69bc75f3a2a8f3075b076186e80af448e81
Shobhit Agarwal <shobhit@capiot.com>
2018-11-15 12:10:37 +0530

    removed modules from index file

--- index.js
+++ index.js
@@ -1,36 +1,20 @@
-var StateEngine = require("./StateEngine");
 var rand = require("./rand");
-var validation = require("./Validation");
 var date = require("./date");
 var IDGenerator = require("./IDGenerator");
-var rbac = require("./rbac");
 var counter = require("./counter");
 var logger = require("./logger");
 var moveToES = require("./moveToES");
-var request = require("./Request");
 var authMiddleware = require("./AuthorizationMiddleware");
 var logMiddleware = require("./logMiddleware");
 var logToMongo = require("./logToMongo");
 
-var masterName = null;
-
-function init(name) {
-    masterName = name;
-    request.init(masterName);
-}
-
 module.exports = {
-    init: init,
-    StateEngine : StateEngine,
     rand :   rand,
     date : date,
     getUniqueID : IDGenerator,
     counter: counter,
     logger : logger,
-    validation : validation,
     moveToES : moveToES,
-    rbac:   rbac,
-    request : request,
 	authMiddleware : authMiddleware,
     logMiddleware: logMiddleware,
     logToMongo: logToMongo	
