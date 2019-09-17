b016941ecb4075a9df8259aab0dd75783088bf52
Shobhit Agarwal <shobhit@appveen.com>
2019-09-17 10:54:56 +0530

    some logger fixes

--- logger/index.js
+++ logger/index.js
@@ -2,6 +2,7 @@ var log4js = require("log4js");
 const logLevel = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'info';
 
 function getLogger(defaultContext) {
+  let req = JSON.parse(JSON.stringify(defaultContext));
   log4js.configure({
     levels: {
       AUDIT: { value: Number.MAX_VALUE - 1, colour: 'yellow' }
@@ -16,19 +17,17 @@ function getLogger(defaultContext) {
             podId: defaultContext.podId,
             user: function (logEvent) {
               let user = defaultContext.user;
-              let req = logEvent.data[0];
+              req = logEvent.data[0];
               if (req && typeof req == 'object') {
                 user = req.get('user') || req.user || user;
-                if (req.get('txnId') == undefined || req.txnId == undefined) logEvent.data.shift();
+                logEvent.data.shift();
               }
               return user;
             },
-            txnId: function (logEvent) {
+            txnId: function () {
               let txnId = defaultContext.txnId;
-              let req = logEvent.data[0]
               if (req && typeof req == 'object') {
-                txnId = req.txnId;
-                logEvent.data.shift();
+                txnId = req.get('txnId') || req.txnId || txnId;
               }
               return txnId;
             }
