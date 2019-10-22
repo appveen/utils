b50e20f6c936a5c60dd8ed30460bd901f2940dce
Shobhit Agarwal <shobhit@appveen.com>
2019-10-22 12:19:02 +0530

    revert logger changes

--- logger/index.js
+++ logger/index.js
@@ -1,46 +1,11 @@
 var log4js = require("log4js");
-const logLevel = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'info';
-
-function getLogger(defaultContext) {
-  let req = JSON.parse(JSON.stringify(defaultContext));
-  log4js.configure({
+const logLevel = process.env.LOG_LEVEL ? process.env.LOG_LEVEL: 'info';
+log4js.configure({
     levels: {
-      AUDIT: { value: Number.MAX_VALUE - 1, colour: 'yellow' }
+      AUDIT: { value: Number.MAX_VALUE-1, colour: 'yellow' }
     },
-    appenders: {
-      out: {
-        type: 'stdout', layout: {
-          type: 'pattern',
-          pattern: '[%d] [%p] [%x{ns}] [%x{podId}] [%x{user}] [%x{txnId}] - %m%n',
-          tokens: {
-            ns: defaultContext.ns,
-            podId: defaultContext.podId,
-            user: function (logEvent) {
-              let user = defaultContext.user;
-              req = logEvent.data[0];
-              if (req && typeof req == 'object') {
-                user = (typeof req.get == 'function' && req.get('user')) || req.user || user;
-                logEvent.data.shift();
-              }
-              return user;
-            },
-            txnId: function () {
-              let txnId = defaultContext.txnId;
-              if (req && typeof req == 'object') {
-                txnId = (typeof req.get == 'function' && req.get('txnId')) || req.txnId || txnId;
-              }
-              return txnId;
-            }
-          }
-        }
-      }
-    },
+    appenders: { out: { type: 'stdout', layout: { type: 'basic' } } },
     categories: { default: { appenders: ['out'], level: logLevel.toUpperCase() } }
   });
-  log4js.level = logLevel;
-  return log4js;
-}
-
-module.exports = {
-  getLogger
-};
+log4js.level = logLevel;
+module.exports.getLogger = log4js;
