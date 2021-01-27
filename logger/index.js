32864ebbecb3b184aaa7e64b53777c41f697b07c
Jerry M <developer.jerrymannel@gmail.com>
2021-01-27 15:05:40 +0530

    Changes - Removed req counter - Added TxnID - Removed AUDIT as a log level

--- logger/index.js
+++ logger/index.js
@@ -1,9 +1,6 @@
 var log4js = require("log4js");
 const logLevel = process.env.LOG_LEVEL ? process.env.LOG_LEVEL: 'info';
 log4js.configure({
-    levels: {
-      AUDIT: { value: Number.MAX_VALUE-1, colour: 'yellow' }
-    },
     appenders: { out: { type: 'stdout', layout: { type: 'basic' } } },
     categories: { default: { appenders: ['out'], level: logLevel.toUpperCase() } }
   });
